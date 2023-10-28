d6c41581fd20d663d94a5e1d251f449e4aeec732
jereycui2008 <jeremycuid@gmail.com>
2023-10-28 16:28:17 -0700

    a

--- src/_01_intro_to_static/Canvas.java
+++ src/_01_intro_to_static/Canvas.java
@@ -57,8 +57,6 @@ public class Canvas {
     }
 
     public void addCircle(Circle circle) {
-    	System.out.println("circle radius = " + Circle.radius);
-        System.out.println("circle color = " + Circle.color);
     	circles.add(circle);
 
         // Must place after adding new circle
