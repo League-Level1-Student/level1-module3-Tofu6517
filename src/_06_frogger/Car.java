47c732bac4980bcc5d7924d2f4148bae228e682f
jereycui2008 <jeremycuid@gmail.com>
2023-11-18 16:27:26 -0800

    a

--- src/_06_frogger/Car.java
+++ src/_06_frogger/Car.java
@@ -1,21 +1,41 @@
 package _06_frogger;
 
+import java.util.Random;
+
 import processing.core.PApplet;
 
-public class Car extends PApplet{
+public class Car{
 	int x;
 	int y;
 	int size;
 	int speed;
-	public Car(int x, int y, int size, int speed) {
+	PApplet processing;
+	public Car(int x, int y, int size, int speed, PApplet processing) {
 		this.x=x;
 		this.y=y;
 		this.size=size;
 		this.speed=speed;
+		this.processing=processing;
 	}
-	void display()
-	  {
-	    fill(0,255,0);
-	    rect(x , y,  size, 50);
+	void display(){
+	    processing.fill(0,255,0);
+	    processing.rect(x , y,  size, 50);
 	  }
+	void movingCarsLeft() {
+		x=x-speed;
+		if(x<=-size) {
+			x=-size;
+		}
+	}
+	void movingCarsRight() {
+		x=x+speed;
+		if(x>=size+800) {
+			x=800+size;
+		}
+	}
+	void CarDirection() {
+		Random rand = new Random(1);
+		return;
+	}
+	
 }
--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -1,12 +1,12 @@
 package _06_frogger;
-
+import java.util.Random;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-    int frogHopX;
-    int frogHopY;
+    int frogX=400;
+    int frogY=570;
     Car car1;
     Car car2;
     Car car3;
@@ -17,19 +17,22 @@ public class Frogger extends PApplet {
 
     @Override
     public void setup() {
-    	car1=new Car(1,200,40,5);
-    	car2=new Car(800,300,40,5);
-    	car3=new Car(1,400,40,5);
+    	car1=new Car(800,200,40,3,this);
+    	car2=new Car(0,300,40,10,this);
+    	car3=new Car(800,400,40,5,this);
     }
 
     @Override
     public void draw() {
     	background(0,0,200);
     	fill(20,20,20);
-    	ellipse(400, 600, 20, 20);
+    	ellipse(frogX,frogY, 20, 20);
     	car1.display();
+    	car1.movingCarsLeft();
     	car2.display();
+    	car2.movingCarsRight();
     	car3.display();
+    	car3.movingCarsLeft();
     }
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
@@ -39,34 +42,34 @@ public class Frogger extends PApplet {
         if(key == CODED){
             if(keyCode == UP)
             {
-                frogHopY+=1;
+                frogY-=5;
             }
             else if(keyCode == DOWN)
             {
-            	 frogHopY-=1;
+            	 frogY+=5;
             }
             else if(keyCode == RIGHT)
             {
-            	 frogHopY+=1;
+            	 frogX+=5;
             }
             else if(keyCode == LEFT)
             {
-            	 frogHopY-=1;
+            	 frogX-=5;
             }
         }
     }
     void outSide() {
-    	if(frogHopY>=600) {
-    		frogHopY-=1;
+    	if(frogY>=600) {
+    		frogY-=1;
     	}
-    	if(frogHopY<=0) {
-    		frogHopY+=1;
+    	if(frogY<=0) {
+    		frogY+=1;
     	}
-    	if(frogHopX>=800) {
-    		frogHopX-=1;
+    	if(frogX>=800) {
+    		frogX-=1;
     	}
-    	if(frogHopY<=0) {
-    		frogHopX+=1;
+    	if(frogY<=0) {
+    		frogX+=1;
     	}
     }
     
